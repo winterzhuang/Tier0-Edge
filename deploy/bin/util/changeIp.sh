cf4e2bf85caf6115e14a5d65a9e5bff58fd32782
zhuangmh <15167160211@163.com>
2026-01-07 10:24:19 +0800

    fix changeIp

--- deploy/bin/util/changeIp.sh
+++ deploy/bin/util/changeIp.sh
@@ -24,8 +24,8 @@ if [ "$ENTRANCE_PROTOCOL" == "https" ]; then
   fi
 fi
 
-KONG_SQL_1="UPDATE \"public\".\"plugins\" SET \"config\" = '{\"login_url\": \"$BASE_URL/keycloak/home/auth/realms/supos/protocol/openid-connect/auth?client_id=supos&redirect_uri=$BASE_URL/inter-api/supos/auth/token&response_type=code&scope=openid\", \"forbidden_url\": \"/403\", \"whitelist_paths\": [\"^/inter-api/supos/auth.*$\", \"^/inter-api/supos/systemConfig.*$\", \"^/inter-api/supos/theme/getConfig.*$\", \"^/$\", \"^/assets.*$\", \"^/locale.*$\", \"^/logo.*$\", \"^/gitea.*git.*$\", \"^/tier0-login.*$\", \"^/403$\", \"^/open-api/.*$\", \"^/keycloak.*$\", \"^/nodered.*$\", \"^/files.*$\", \"^/freeLogin.*$\", \"^/inter-api/supos/dev/logs.*$\", \"^/inter-api/supos/license.*$\", \"^/inter-api/supos/cascade.*$\"], \"enable_deny_check\": true, \"enable_resource_check\": true}', \"updated_at\" = NOW() WHERE \"name\" = 'supos-auth-checker';"
-KONG_SQL_2="UPDATE \"public\".\"plugins\" SET \"config\" = '{\"add\": {\"body\": [], \"headers\": [], \"querystring\": []}, \"append\": {\"body\": [], \"headers\": [], \"querystring\": [\"client_id:supos\", \"response_type:code\", \"scope:openid\", \"redirect_uri:$BASE_URL/inter-api/supos/auth/token\"]}, \"remove\": {\"body\": [], \"headers\": [], \"querystring\": []}, \"rename\": {\"body\": [], \"headers\": [], \"querystring\": []}, \"replace\": {\"uri\": null, \"body\": [], \"headers\": [], \"querystring\": []}, \"http_method\": null}' WHERE \"name\" = 'request-transformer';"
+KONG_SQL_1="UPDATE \"public\".\"plugins\" SET \"config\" = '{\"login_url\": \"$BASE_URL/keycloak/home/auth/realms/tier0/protocol/openid-connect/auth?client_id=tier0&redirect_uri=$BASE_URL/inter-api/supos/auth/token&response_type=code&scope=openid\", \"forbidden_url\": \"/403\", \"whitelist_paths\": [\"^/inter-api/supos/auth.*$\", \"^/inter-api/supos/systemConfig.*$\", \"^/inter-api/supos/theme/getConfig.*$\", \"^/$\", \"^/assets.*$\", \"^/locale.*$\", \"^/logo.*$\", \"^/gitea.*git.*$\", \"^/tier0-login.*$\", \"^/403$\", \"^/open-api/.*$\", \"^/keycloak.*$\", \"^/nodered.*$\", \"^/files.*$\", \"^/freeLogin.*$\", \"^/inter-api/supos/dev/logs.*$\", \"^/inter-api/supos/license.*$\", \"^/inter-api/supos/cascade.*$\"], \"enable_deny_check\": true, \"enable_resource_check\": true}', \"updated_at\" = NOW() WHERE \"name\" = 'supos-auth-checker';"
+KONG_SQL_2="UPDATE \"public\".\"plugins\" SET \"config\" = '{\"add\": {\"body\": [], \"headers\": [], \"querystring\": []}, \"append\": {\"body\": [], \"headers\": [], \"querystring\": [\"client_id:tier0\", \"response_type:code\", \"scope:openid\", \"redirect_uri:$BASE_URL/inter-api/supos/auth/token\"]}, \"remove\": {\"body\": [], \"headers\": [], \"querystring\": []}, \"rename\": {\"body\": [], \"headers\": [], \"querystring\": []}, \"replace\": {\"uri\": null, \"body\": [], \"headers\": [], \"querystring\": []}, \"http_method\": null}' WHERE \"name\" = 'request-transformer';"
 KEYCLOAK_SQL="UPDATE \"public\".\"client\" SET \"base_url\" = '$BASE_URL/home', \"management_url\" = '$BASE_URL', \"root_url\" = '$BASE_URL'  WHERE \"id\" = 'a7b53e5e-3567-470a-9da1-94cc0c7f18e6';"
 
 docker exec -i -e PGPASSWORD=postgres postgresql  psql -U postgres -d kong -c "$KONG_SQL_1" >/dev/null
@@ -44,7 +44,7 @@ docker rm -f uns
 docker rm -f kong
 
 DOCKER_COMPOSE_FILE=$SCRIPT_DIR/../../docker-compose.yml
-docker compose --env-file $ENV_FILE --env-file $SCRIPT_DIR/../../.env.tmp -p supos -f $DOCKER_COMPOSE_FILE up -d uns kong
+docker compose --env-file $ENV_FILE --env-file $SCRIPT_DIR/../../.env.tmp -p tier0 -f $DOCKER_COMPOSE_FILE up -d uns kong
 
 sleep 15s
 
